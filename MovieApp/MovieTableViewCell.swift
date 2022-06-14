7ab5f463d61aad2030470dfa2dd5532fee0ddda4
juanemporis <juanemporis@gmail.com>
2022-06-14 18:45:52 -0500

    Se agregaron los StackView, Label y los imageView en el Main y se creo un archivo CocoaTouch  MovieTableViewCell

---
+++ MovieApp/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieApp
+//
+//  Created by levi on 14/06/22.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
