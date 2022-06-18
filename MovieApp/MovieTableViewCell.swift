67a2d95d6961486e8fed7a87aabc9caf13b0470b
juanemporis <juanemporis@gmail.com>
2022-06-17 20:35:11 -0500

    Se agregaron los Oulets creados del Main y se introdujo código para poder obtener los datos de la URL

--- MovieApp/MovieTableViewCell.swift
+++ MovieApp/MovieTableViewCell.swift
@@ -9,15 +9,50 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var moviePoster: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieYear: UILabel!
+    @IBOutlet weak var movieOverView: UILabel!
+    @IBOutlet weak var movieRate: UILabel!
+    
+    private var urlString: String = ""
+    
+    
+    private func updateUI(title : String?, releaseDate : String?, rating : Double?, overview : String?, poster : String?  ) {
+        
+        self .movieTitle.text = title
+        self .movieYear.text = convertDateFormater(releaseDate)
+        guard let rate = rating else {
+            return
+        }
+        self .movieYear.text = String (rate)
+        self .movieOverView.text = overview
+        
+        guard let posterString = poster else {
+            return
+        }
+        urlString = "https://image.tmdb.org/t/p/w300" + posterString
+        
+        guard let posterImageURL = URL(string: urlString) else {
+            self.moviePoster.image = UIImage(named: "noImageAvaidable")
+            return
+        }
+        
+        self.moviePoster.image = nil
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func convertDateFormater (_ date : String?) -> String {
+        var fixDate = ""
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        if let originalDate = date {
+            if let newDate = dateFormatter.date(from: originalDate) {
+                dateFormatter.dateFormat = "dd.MM.yyyy"
+                fixDate = dateFormatter.string(from: newDate)
+            }
+        }
+         return fixDate
+        
     }
-
 }
